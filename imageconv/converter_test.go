6599b85cdaaaab0244ca90d5982ce29af2a77380
ogataka50 <ogataka50@gmail.com>
2018-05-31 02:04:30 +0900

    fix tests テーブル駆動テストを行う,テストヘルパーを作ってみる

--- imageconv/converter_test.go
+++ imageconv/converter_test.go
@@ -8,11 +8,34 @@ import (
 )
 
 func TestConverter_Convert(t *testing.T) {
-	c := imageconv.Converter{
-		Path:    "../testdata/sample.png",
-		FromExt: "png",
-		ToExt:   "jpg",
+	cases := []struct {
+		name    string
+		path    string
+		fromExt string
+		toExt   string
+	}{
+		{name: "pngTojpg", path: "../testdata/sample_png.png", fromExt: "png", toExt: "jpg"},
+		{name: "pngTogif", path: "../testdata/sample_png.png", fromExt: "png", toExt: "gif"},
+		{name: "gifTopng", path: "../testdata/sample_gif.gif", fromExt: "gif", toExt: "png"},
+		{name: "gifTojpg", path: "../testdata/sample_gif.gif", fromExt: "gif", toExt: "jpg"},
+		{name: "jpgTopng", path: "../testdata/sample_jpg.jpg", fromExt: "jpg", toExt: "png"},
+		{name: "jpgTogif", path: "../testdata/sample_jpg.jpg", fromExt: "jpg", toExt: "gif"},
 	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			conv := imageconv.Converter{
+				Path:    c.path,
+				FromExt: c.fromExt,
+				ToExt:   c.toExt,
+			}
+			testConvert(t, conv)
+		})
+	}
+}
+
+func testConvert(t *testing.T, c imageconv.Converter) {
+	t.Helper()
 
 	convertedPath, err := c.Convert()
 	if err != nil {
--- imageconv/finder_test.go
+++ imageconv/finder_test.go
@@ -10,74 +10,84 @@ import (
 )
 
 func TestFinder_IsDir_Success(t *testing.T) {
-	testData := []struct {
+	cases := []struct {
+		name   string
 		dir    string
 		ext    string
 		expect bool
 	}{
-		{"../testdata", "jpg", true},
-		{"../imageconv", "jpg", true},
-		{"../../go-imageconv", "jpg", true},
+		{name: "../testdata", dir: "../testdata", ext: "jpg", expect: true},
+		{name: "../imageconv", dir: "../imageconv", ext: "jpg", expect: true},
+		{name: "../../go-imageconv", dir: "../../go-imageconv", ext: "jpg", expect: true},
 	}
 
-	for _, d := range testData {
-		f := imageconv.Finder{
-			Dir: d.dir,
-			Ext: d.ext,
-		}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			f := imageconv.Finder{
+				Dir: c.dir,
+				Ext: c.ext,
+			}
 
-		expect := d.expect
-		actual, err := f.IsDir()
-		if actual != expect || err != nil {
-			t.Errorf(`expect="%v" actual="%v"`, expect, actual)
-		}
+			expect := c.expect
+			actual, err := f.IsDir()
+			if actual != expect || err != nil {
+				t.Errorf(`expect="%v" actual="%v"`, expect, actual)
+			}
+		})
 	}
 }
 
 func TestFinder_IsDir_Error(t *testing.T) {
-	testData := []struct {
+	cases := []struct {
+		name   string
 		dir    string
 		ext    string
 		expect bool
 		err    error
 	}{
-		{"../xxx", "jpg", false, errors.New("stat ../xxx: no such file or directory")},
-		{"../testdata/sample.png", "png", false, nil},
+		{name: "../xxx", dir: "../xxx", ext: "jpg", err: errors.New("stat ../xxx: no such file or directory")},
+		{name: "../xxx", dir: "../testdata/sample_png.png", ext: "png"},
 	}
 
-	for _, d := range testData {
-		f := imageconv.Finder{
-			Dir: d.dir,
-			Ext: d.ext,
-		}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			f := imageconv.Finder{
+				Dir: c.dir,
+				Ext: c.ext,
+			}
 
-		expect := d.expect
-		actual, err := f.IsDir()
-		if actual != expect || (err != nil && err.Error() != d.err.Error()) {
-			t.Errorf(`expect="%v" actual="%v" err="%v"`, expect, actual, err)
-		}
+			expect := c.expect
+			actual, err := f.IsDir()
+			if actual != expect || (err != nil && err.Error() != c.err.Error()) {
+				t.Errorf(`expect="%v" actual="%v" err="%v"`, expect, actual, err)
+			}
+		})
 	}
 }
 
 func TestFinder_FindByExt_Success(t *testing.T) {
-	testData := []struct {
+	cases := []struct {
+		name       string
 		dir        string
 		ext        string
 		expectList []string
 	}{
-		{"../testdata", "png", []string{"../testdata/sample.png"}},
+		{name: "../testdsata", dir: "../testdata", ext: "png", expectList: []string{"../testdata/sample_png.png"}},
 	}
 
-	for _, d := range testData {
-		f := imageconv.Finder{
-			Dir: d.dir,
-			Ext: d.ext,
-		}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
 
-		expect := d.expectList
-		actual, err := f.FindByExt()
-		if !reflect.DeepEqual(expect, actual) || err != nil {
-			t.Errorf(`expect="%v" actual="%v" err="%v"`, expect, actual, err)
-		}
+			f := imageconv.Finder{
+				Dir: c.dir,
+				Ext: c.ext,
+			}
+
+			expect := c.expectList
+			actual, err := f.FindByExt()
+			if !reflect.DeepEqual(expect, actual) || err != nil {
+				t.Errorf(`expect="%v" actual="%v" err="%v"`, expect, actual, err)
+			}
+		})
 	}
 }
